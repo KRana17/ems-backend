38ea878ad9933955a7d28e73f27669cc0e7f7dd1
KRana17 <rr.khevna@gmail.com>
2025-02-22 00:17:50 -0400

    Added docker compose config plusUpdate .gitignore and remove ignored files

--- src/main/java/com/example/ems/EmsBackendApplication.java
+++ src/main/java/com/example/ems/EmsBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.ems")
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
